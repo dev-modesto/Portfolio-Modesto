665a5a8776c010a5de328dd6e387576b711d13c1
dev-modesto <gabriel_26@outlook.com.br>
2024-08-11 18:25:41 -0300

    feat: tabela js datatable

---
+++ js/table.js
@@ -0,0 +1,12 @@
+new DataTable('#myTable', {
+    pagingType: 'simple_numbers',
+    language: {
+        url: '//cdn.datatables.net/plug-ins/2.0.8/i18n/pt-BR.json',
+    },
+
+});
+
+$(document).ready( function () {
+    $('#myTable').DataTable();
+} );
+
