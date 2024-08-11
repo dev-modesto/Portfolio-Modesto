bf264454f11703b4ccc5b8852c0f42f50dd0952c
dev-modesto <gabriel_26@outlook.com.br>
2024-08-11 18:21:35 -0300

    feat: arquivo conexão db

---
+++ config/conexao.php
@@ -0,0 +1,16 @@
+<?php
+
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/portfolio-modesto/vendor/autoload.php';
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+
+    $con = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], $_ENV['DB_DATABASE']);
+    // echo 'conexao estabelecida com sucesso!';
+
+    header("Content-Type:text/html; charset=utf-8");
+    mysqli_query($con,"SET NAMES 'utf8'");
+    mysqli_query($con,"SET character_set_connection = utf8");
+    mysqli_query($con,"SET character_set_client = utf8");
+    mysqli_query($con,"SET character_set_results = utf8");
+
+?>
