ac8d6eb972c9f37308aaf1880bf50a5bc1073a6b
dev-modesto <gabriel_26@outlook.com.br>
2024-08-18 00:45:53 -0300

    feat: modal utilitário

---
+++ js/modal.js
@@ -0,0 +1,27 @@
+function abrirModal(botaoClick, classIdTabela, idDataPesquisa, urlCaminho, classClickTrue, classModal, idModal) {
+    $(document).ready(function () {
+        $(document).on('click', botaoClick, function (e) {
+            e.preventDefault();
+            var idPrincipal = $(this).closest(classIdTabela).data(idDataPesquisa);
+
+            $.ajax({
+                type: "POST",
+                url: urlCaminho,
+                data: {
+                    [classClickTrue]: true,
+                    idPrincipal: idPrincipal,
+                },
+                success: function (response) {
+                    $(classModal).html(response);
+                    $(idModal).modal('show');
+                }
+            });
+        });
+    });
+}
+
+abrirModal('.btn-excluir-formacao', 'tr', 'id-formacao', 'include/cModalExcluirFormacao.php', 'click-excluir-formacao', '.modalExcluir', '#modalExcluir');
+abrirModal('.btn-editar-formacao', 'tr', 'id-formacao', 'include/cModalEditarFormacao.php', 'click-editar-formacao', '.modalEditarFormacao', '#modalEditarFormacao');
+
+abrirModal('.btn-excluir-autor', 'tr', 'id-autor', 'include/cModalExcluirAutor.php', 'click-excluir-autor', '.modalExcluir', '#modalExcluir');
+abrirModal('.btn-editar-autor', 'tr', 'id-autor', 'include/cModalEditarAutor.php', 'click-editar-autor', '.modalEditarAutor', '#modalEditarAutor');
