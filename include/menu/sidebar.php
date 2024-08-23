c10b3a42f142b92070f9424f4e5a666b8d8b6a18
dev-modesto <gabriel_26@outlook.com.br>
2024-08-22 23:34:36 -0300

    feat: caminho link pagina sidebar

--- include/menu/sidebar.php
+++ include/menu/sidebar.php
@@ -42,12 +42,12 @@
     </div>
 
     <ul class="navbar-itens">
-        <li class="cor-8"><a href="#" class="font-1-s" aria-disabled="true"><span class="material-symbols-rounded">dashboard</span><p class="texto-nav">Projetos</p></a></li>
-        <li class="cor-3"><a href="#" class="font-1-s"><span class="material-symbols-rounded">school</span><p class="texto-nav">Formação</p></a></li>
-        <li class="cor-8"><a href="#" class="font-1-s" aria-disabled="true"><span class="material-symbols-rounded">polyline</span><p class="texto-nav">Tecnologias</p></a></li>
-        <li class="cor-3"><a href="#" class="font-1-s"><span class="material-symbols-rounded">group</span><p class="texto-nav">Autores</p></a></li>
-        <li class="cor-3"><a href="#" class="font-1-s"><span class="material-symbols-rounded">assignment</span><p class="texto-nav">Áreas</p></a></li>
-        <li class="cor-3"><a href="#" class="font-1-s"><span class="material-symbols-rounded">image</span><p class="texto-nav">Imagens</p></a></li>
+        <li class="cor-8"><a href="<?php echo BASE_URL ?>/app/administracao/projeto/" class="font-1-s"><span class="material-symbols-rounded">dashboard</span><p class="texto-nav">Projetos</p></a></li>
+        <li class="cor-3"><a href="<?php echo BASE_URL ?>/app/administracao/formacao/" class="font-1-s"><span class="material-symbols-rounded">school</span><p class="texto-nav">Formação</p></a></li>
+        <li class="cor-8"><a href="<?php echo BASE_URL ?>/app/administracao/tecnologia/" class="font-1-s"><span class="material-symbols-rounded">polyline</span><p class="texto-nav">Tecnologias</p></a></li>
+        <li class="cor-3"><a href="<?php echo BASE_URL ?>/app/administracao/autor/" class="font-1-s"><span class="material-symbols-rounded">group</span><p class="texto-nav">Autores</p></a></li>
+        <li class="cor-3"><a href="<?php echo BASE_URL ?>/app/administracao/areaFormacao/" class="font-1-s"><span class="material-symbols-rounded">assignment</span><p class="texto-nav">Áreas</p></a></li>
+        <li class="cor-3"><a href="<?php echo BASE_URL ?>/app/administracao/imagem/" class="font-1-s"><span class="material-symbols-rounded">image</span><p class="texto-nav">Imagens</p></a></li>
     </ul>
 </nav>
 
