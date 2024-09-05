31138cf8e954f5aa02421ff5f3ec6e4eacfc65ac
dev-modesto <gabriel_26@outlook.com.br>
2024-09-04 22:41:18 -0300

    feat: atualizando as imagens do projeto

--- app/administracao/projeto/include/aProjeto.php
+++ app/administracao/projeto/include/aProjeto.php
@@ -1,5 +1,8 @@
 <?php 
     include $_SERVER['DOCUMENT_ROOT'] . "/Portfolio-Modesto/config/base.php";
+    include BASE_PATH . "/funcoes/funcaoImagem.php";
+    include BASE_PATH . "/include/funcoes/db-queries/projeto.php";
+
     ARQUIVO_CONEXAO;
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -32,6 +35,80 @@
             echo json_encode($mensagem);
             die();
         } 
+
+        $imagensEnvio = [
+            'imagem-projeto' => [
+                'categoria' => 'projeto',
+                'texto-alternativo' => $textoAlternativo
+            ],
+
+            'logo-projeto' => [
+                'categoria' => 'logo',
+                'texto-alternativo' => ''
+            ]
+        ];
+        
+        $caminhoRelativo = "/assets/img/projetos/";
+        $caminhoAbsoluto = "/Portfolio-Modesto/assets/img/projetos/";
+        $caminhoPasta = $_SERVER['DOCUMENT_ROOT'] . $caminhoAbsoluto;
+
+        
+        foreach ($imagensEnvio as $chaveImagem => $valorImagem) {
+            if (!empty($_FILES[$chaveImagem]['name'])) {
+
+                $categoria = $valorImagem['categoria'];
+                $textoAlt = $valorImagem['texto-alternativo'];
+
+                $cProjetoImagem = cProjetoImagem($con, $idProjeto, $categoria);
+                $arrayImagem = mysqli_fetch_assoc($cProjetoImagem);
+
+                $caminhoRelativoImagem = $arrayImagem['caminho_original'];
+                $caminhoAbsolutoImagem = BASE_PATH . $caminhoRelativoImagem;
+                $idImagem = $arrayImagem['id_imagem'];
+
+                excluirImagemPasta($caminhoAbsolutoImagem);
+
+                $sqlImagem = mysqli_prepare($con, "DELETE FROM tbl_imagem WHERE id_imagem = ?");
+                mysqli_stmt_bind_param($sqlImagem, "i", $idImagem);
+                mysqli_stmt_execute($sqlImagem);
+        
+                $imagemSalva = salvarImagem($_FILES[$chaveImagem], $caminhoRelativo, $caminhoPasta);
+        
+                $imagens = [
+                    'nome' => $imagemSalva['nome'],
+                    'caminho' => $imagemSalva['caminho'],
+                    'texto-alternativo' => $textoAlt,
+                    'categoria' => $categoria
+                ];
+        
+                $sqlAdicionaImagem = mysqli_prepare(
+                    $con,
+                    "INSERT INTO tbl_imagem (nome_original, caminho_original, texto_alt, categoria)
+                    VALUES (?, ?, ?, ?)"
+                );
+        
+                mysqli_stmt_bind_param(
+                    $sqlAdicionaImagem, 
+                    "ssss", 
+                    $imagens['nome'],
+                    $imagens['caminho'],
+                    $imagens['texto-alternativo'],
+                    $imagens['categoria']
+                );
+        
+                mysqli_stmt_execute($sqlAdicionaImagem);
+                $idImagem = mysqli_insert_id($con);
+        
+                $sqlImagemProjeto = mysqli_prepare(
+                    $con, 
+                    "INSERT INTO tbl_imagem_projeto (id_projeto, id_imagem)
+                    VALUES (?, ?)"
+                );
+        
+                mysqli_stmt_bind_param($sqlImagemProjeto, "ii", $idProjeto, $idImagem);
+                mysqli_stmt_execute($sqlImagemProjeto);
+            }
+        }
 
         mysqli_begin_transaction($con);
 
