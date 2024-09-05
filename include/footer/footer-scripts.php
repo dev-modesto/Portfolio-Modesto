322f4b8f0652bbe2de9f78c97334f2cb5c40e51d
dev-modesto <gabriel_26@outlook.com.br>
2024-09-04 23:29:43 -0300

    feat: link menu mobile

--- include/footer/footer-scripts.php
+++ include/footer/footer-scripts.php
@@ -1,5 +1,6 @@
 <?php 
     include_once $_SERVER['DOCUMENT_ROOT'] . '/Portfolio-Modesto/config/config.php';
+    
 ?>
    
     <!-- arquivos js importação bibliotecas -->
@@ -8,3 +9,4 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js" integrity="sha512-0XDfGxFliYJPFrideYOoxdgNIvrwGTLnmK20xZbCAvPfLGQMzHUsaqZK8ZoH+luXGRxTrS46+Aq400nCnAT0/w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="<?php echo BASE_URL . '/js/filtro-certificados.js'?>"></script>
+    <script src="<?php echo BASE_URL . '/js/menu-mobile.js'?>"></script>
