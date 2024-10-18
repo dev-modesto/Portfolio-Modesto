76d5a7dfd234e66b172002fd405cf4e987ee7a10
Gabriel.Modesto <gabriel_26@outlook.com.br>
2024-10-18 11:27:21 -0300

    feat: captura exceções ao gravar projeto

--- app/administracao/projeto/include/gProjeto.php
+++ app/administracao/projeto/include/gProjeto.php
@@ -1,111 +1,108 @@
 <?php 
     include '../../../../config/base.php';
     include BASE_PATH . '/funcoes/funcaoImagem.php';
+    include BASE_PATH . '/include/funcoes/diversas/respostaJson.php';
     
     if(isset($_POST['tecnologias'])) {
 
         $sqlMeuIdAutor = "SELECT id_autor FROM tbl_autor WHERE nome LIKE '%gabriel modesto%'";
         $consultaMeuId = mysqli_query($con, $sqlMeuIdAutor);
         $resultadoConsultaMeuId = mysqli_num_rows($consultaMeuId);
-
-        if ($resultadoConsultaMeuId == 0) {
-            $mensagem['mensagem'] = 'O autor Gabriel Modesto não foi encontrado.';
-            header('Content-Type: application/json');
-            echo json_encode($mensagem);
-            die();
-        }
-
-        $tecnologias = explode(',', $_POST['tecnologias']);
-        $autores = explode(',', $_POST['autores']);
-
-        $arrayConsultaMeuId = mysqli_fetch_assoc($consultaMeuId);
-        $meuIdAutor = $arrayConsultaMeuId['id_autor'];
-
-        array_unshift($autores, $meuIdAutor);
-        $arrayAutores = array_filter($autores, function($array) {
-            return !empty($array);
-        });
-
-        $nomeProjeto = trim($_POST['nome-projeto']);
-        $idCategoria = $_POST['id-categoria-projeto'];
-
-        if (is_numeric($idCategoria)) {
-            $idCategoria = intval($idCategoria);
         
-        } else {
-            $mensagem['mensagem'] = 'Ocorreu um erro. Não foi possível prosseguir com o cadastro do projeto.';
-            header('Content-Type: application/json');
-            echo json_encode($mensagem);
-            die();
-        }
-
-        $tipoProjeto = $_POST['tipo-projeto'];
-        $descricaoProjeto = $_POST['descricao-projeto'];
-        $descricaoTipoProjeto = $_POST['descricao-tipo-projeto'];
-        $dataDesenvolvimento = trim($_POST['data-desenvolvimento']);
-        $nomeTituloImgThumbnail = trim($_POST['nome-titulo-img-thumbnail']);
-        $nomeTituloImgLogo = trim($_POST['nome-titulo-img-logo']);
-        $textoAltImgLogo = trim($_POST['texto-alt-logo']);
-        $textoAltImgThumbnail = trim($_POST['texto-alt-thumbnail']);
-
-        $statusGeralProjeto = $_POST['status-geral-projeto'];
-        $projetoDestaque = $_POST['projeto-destaque'];
-        $statusProjeto = $_POST['status-progresso-projeto'];
-        $projetoEquipe = $_POST['projeto-equipe'];
-
-        $caminhoRelativo = "/assets/img/projetos/";
-        $caminhoAbsoluto = BASE_PATH . "/assets/img/projetos/";
-        $caminhoPasta = $caminhoAbsoluto;
-        
-        $resultadoImagens = []; 
-        $resultadoImagens['logo'] = salvarImagem($_FILES['logo-projeto'], $caminhoRelativo, $caminhoPasta);
-        $resultadoImagens['projeto'] = salvarImagem($_FILES['imagem-projeto'], $caminhoRelativo, $caminhoPasta);
-        
-        $mensagem = [];
-        foreach ($resultadoImagens as $chave => $valor) {
-
-            if (is_string($valor)) {
-
-                foreach ($resultadoImagens as $imagem) {
-                    if (isset($imagem['caminho'])) {
-                        $caminhoImagem = $imagem['caminho'];
-                        $caminhoAbsolutoImgSemErro = BASE_PATH . $caminhoImagem;
-                        excluirImagemPasta($caminhoAbsolutoImgSemErro);
-                    } 
-                }
-
-                $mensagem['mensagem'] = $valor;
-                header('Content-Type: application/json');
-                echo json_encode($mensagem);
-                die();
-            }
-        }
-
-        $imagens = [
-            [
-                'nome-titulo' => $nomeTituloImgThumbnail,
-                'nome' => $resultadoImagens['projeto']['nome'],
-                'caminho' => $resultadoImagens['projeto']['caminho'],
-                'texto-alternativo' => $textoAltImgThumbnail,
-                'categoria' => 'projeto',
-                'tipo-imagem' => 'thumbnail',
-            ],
-            [
-                'nome-titulo' => $nomeTituloImgLogo,
-                'nome' => $resultadoImagens['logo']['nome'],
-                'caminho' => $resultadoImagens['logo']['caminho'],
-                'texto-alternativo' => $textoAltImgLogo,
-                'categoria' => 'projeto',
-                'tipo-imagem' => 'logo',
-            ]
-        ];
-
-        $linkDeploy = trim($_POST['link-deploy']);
-        $linkFigma = trim($_POST['link-figma']);
-        $linkRepositorio = trim($_POST['link-repositorio']);
-        
-        mysqli_begin_transaction($con);
         try {
+
+            if ($resultadoConsultaMeuId == 0) {
+                $mensagem['mensagem'] = 'O autor Gabriel Modesto não foi encontrado.';
+                throw new Exception(json_encode($mensagem));
+            }
+
+            $tecnologias = explode(',', $_POST['tecnologias']);
+            $autores = explode(',', $_POST['autores']);
+
+            $arrayConsultaMeuId = mysqli_fetch_assoc($consultaMeuId);
+            $meuIdAutor = $arrayConsultaMeuId['id_autor'];
+
+            array_unshift($autores, $meuIdAutor);
+            $arrayAutores = array_filter($autores, function($array) {
+                return !empty($array);
+            });
+
+            $nomeProjeto = trim($_POST['nome-projeto']);
+            $idCategoria = $_POST['id-categoria-projeto'];
+
+            if (is_numeric($idCategoria)) {
+                $idCategoria = intval($idCategoria);
+            
+            } else {
+                $mensagem['mensagem'] = 'Ocorreu um erro. Não foi possível prosseguir com o cadastro do projeto.';
+                throw new Exception(json_encode($mensagem));
+            }
+
+            $tipoProjeto = $_POST['tipo-projeto'];
+            $descricaoProjeto = $_POST['descricao-projeto'];
+            $descricaoTipoProjeto = $_POST['descricao-tipo-projeto'];
+            $dataDesenvolvimento = trim($_POST['data-desenvolvimento']);
+            $nomeTituloImgThumbnail = trim($_POST['nome-titulo-img-thumbnail']);
+            $nomeTituloImgLogo = trim($_POST['nome-titulo-img-logo']);
+            $textoAltImgLogo = trim($_POST['texto-alt-logo']);
+            $textoAltImgThumbnail = trim($_POST['texto-alt-thumbnail']);
+
+            $statusGeralProjeto = $_POST['status-geral-projeto'];
+            $projetoDestaque = $_POST['projeto-destaque'];
+            $statusProjeto = $_POST['status-progresso-projeto'];
+            $projetoEquipe = $_POST['projeto-equipe'];
+
+            $caminhoRelativo = "/assets/img/projetos/";
+            $caminhoAbsoluto = BASE_PATH . "/assets/img/projetos/";
+            $caminhoPasta = $caminhoAbsoluto;
+            
+            $resultadoImagens = []; 
+            $resultadoImagens['logo'] = salvarImagem($_FILES['logo-projeto'], $caminhoRelativo, $caminhoPasta);
+            $resultadoImagens['projeto'] = salvarImagem($_FILES['imagem-projeto'], $caminhoRelativo, $caminhoPasta);
+        
+            $mensagem = [];
+            foreach ($resultadoImagens as $chave => $valor) {
+
+                if (is_string($valor)) {
+
+                    foreach ($resultadoImagens as $imagem) {
+                        if (isset($imagem['caminho'])) {
+                            $caminhoImagem = $imagem['caminho'];
+                            $caminhoAbsolutoImgSemErro = BASE_PATH . $caminhoImagem;
+                            excluirImagemPasta($caminhoAbsolutoImgSemErro);
+                        } 
+                    }
+
+                    $mensagem['mensagem'] = $valor;
+                    throw new Exception(json_encode($mensagem));
+                }
+            }
+
+            $imagens = [
+                [
+                    'nome-titulo' => $nomeTituloImgThumbnail,
+                    'nome' => $resultadoImagens['projeto']['nome'],
+                    'caminho' => $resultadoImagens['projeto']['caminho'],
+                    'texto-alternativo' => $textoAltImgThumbnail,
+                    'categoria' => 'projeto',
+                    'tipo-imagem' => 'thumbnail',
+                ],
+                [
+                    'nome-titulo' => $nomeTituloImgLogo,
+                    'nome' => $resultadoImagens['logo']['nome'],
+                    'caminho' => $resultadoImagens['logo']['caminho'],
+                    'texto-alternativo' => $textoAltImgLogo,
+                    'categoria' => 'projeto',
+                    'tipo-imagem' => 'logo',
+                ]
+            ];
+
+            $linkDeploy = trim($_POST['link-deploy']);
+            $linkFigma = trim($_POST['link-figma']);
+            $linkRepositorio = trim($_POST['link-repositorio']);
+            
+            mysqli_begin_transaction($con);
+        
 
             $sqlProjeto = mysqli_prepare(
                 $con,
@@ -217,26 +214,20 @@
             }
     
             mysqli_commit($con);
-            $mensagem['sucesso'] = true;
-            $mensagem['mensagem'] = "Projeto gravado com sucesso!";
-            header('Content-Type: application/json');
-            echo json_encode($mensagem);
+            $mensagem = ['sucesso' => true, 'mensagem' => 'Projeto gravado com sucesso!'];
+            respostaJson($mensagem);
         
         } catch (Exception $e) {
             mysqli_rollback($con);
-            $mensagem['mensagem'] = "Ocorreu um erro: " . $e->getMessage();
-            header('Content-Type: application/json');
-            echo json_encode($mensagem);
+            $respDecodificada = json_decode($e -> getMessage(), true);
+            respostaJson($respDecodificada);
         
         } finally {
             mysqli_close($con);
         }
 
     } else {
-        $mensagem['mensagem'] = 'Nenhuma tecnologia foi escolhida.';
-        header('Content-Type: application/json');
-        echo json_encode($mensagem);
-        mysqli_close($con);
+        header('location: ../index.php');
     }
 ?>
     
