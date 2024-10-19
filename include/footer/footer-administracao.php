06c262d1e9255c1eabce0387c74bbaca47077e0b
dev-modesto <gabriel_26@outlook.com.br>
2024-10-19 19:20:49 -0300

    fix: identifica dataTable por classe

--- include/footer/footer-administracao.php
+++ include/footer/footer-administracao.php
@@ -1,24 +1,20 @@
 <?php 
-    include_once '../../../config/base.php';
+    // include_once '../../../config/base.php';
 ?>
 
-<script src="<?php echo BASE_URL . '/js/menu.js'?>"></script>
-<script src="<?php echo BASE_URL . '/js/pre-loader.js'?>"></script>
+    <script src="<?php echo BASE_URL . '/js/menu.js'?>"></script>
+    <script src="<?php echo BASE_URL . '/js/pre-loader.js'?>"></script>
 
-
-    <!-- arquivos js importação bibliotecas -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js" integrity="sha512-0XDfGxFliYJPFrideYOoxdgNIvrwGTLnmK20xZbCAvPfLGQMzHUsaqZK8ZoH+luXGRxTrS46+Aq400nCnAT0/w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-    <!-- databtables -->
     <script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
 
     <script src="<?php echo BASE_URL . '/js/modal.js'?>"></script>
     <script>
 
-        new DataTable('#myTable', {
+        new DataTable('.myTable', {
             pagingType: 'simple_numbers',
             language: {
                 url: '<?php echo BASE_URL ?>/config/pt_br.json'
@@ -26,7 +22,7 @@
         });
 
         $(document).ready( function () {
-            $('#myTable').DataTable();
+            $('.myTable').DataTable();
         } );
     </script>
 </body>
