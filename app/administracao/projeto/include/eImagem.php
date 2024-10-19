6c21fc2122316b104808f07a29fe430ce6b45986
dev-modesto <gabriel_26@outlook.com.br>
2024-10-19 00:57:01 -0300

    feat: exclui imagem da galeria

---
+++ app/administracao/projeto/include/eImagem.php
@@ -0,0 +1,55 @@
+<?php 
+    include '../../../../config/base.php';
+    include BASE_PATH . '/funcoes/funcaoImagem.php';
+    include BASE_PATH . '/include/funcoes/db-queries/projeto.php';
+    session_start();
+
+    if ($_SERVER['REQUEST_METHOD'] = 'POST') {
+        $idImagem = $_POST['id-imagem'];
+
+        $sql = mysqli_prepare($con, "SELECT * FROM tbl_imagem_projeto WHERE id_imagem = ?");
+        mysqli_stmt_bind_param($sql, "i", $idImagem);
+        mysqli_stmt_execute($sql);
+        $resultado = mysqli_stmt_get_result($sql);
+        $arrayImagemProjeto = mysqli_fetch_assoc($resultado);
+        $idProjeto = $arrayImagemProjeto['id_projeto'];
+
+        if(is_numeric($idImagem)) {
+            $idImagem = intval($idImagem);
+
+        } else {
+            $mensagem = 'Ocorreu um erro. Não foi possível prosseguir com a exclusão.';
+            header('location: ../include/galeriaProjeto.php?click-galeria-projeto=true&id-projeto=' . $idProjeto . '&msg=' . $mensagem);
+            die();
+        }
+
+        $sqlImagemConsulta = mysqli_prepare(
+            $con, 
+            "SELECT 
+                id_imagem,
+                caminho_original
+            FROM tbl_imagem
+            WHERE id_imagem = ? 
+            ");
+
+        mysqli_stmt_bind_param($sqlImagemConsulta, "i", $idImagem);
+        mysqli_stmt_execute($sqlImagemConsulta);
+        $result = mysqli_stmt_get_result($sqlImagemConsulta);
+        $array = mysqli_fetch_assoc($result);
+
+        $caminhoImagemOriginalRelativo = $array['caminho_original'];
+        $caminhoImagemOriginalAbsoluto = BASE_PATH . $caminhoImagemOriginalRelativo;
+
+        $sql = mysqli_prepare($con, "DELETE FROM tbl_imagem WHERE id_imagem = ?");
+        mysqli_stmt_bind_param($sql, "i", $idImagem);
+        mysqli_stmt_execute($sql);
+
+        excluirImagemPasta($caminhoImagemOriginalAbsoluto);
+        $mensagem = 'Excluído com sucesso!';
+        header('location: ../include/galeriaProjeto.php?click-galeria-projeto=true&id-projeto=' . $idProjeto . '&msg=' . $mensagem);
+
+    } else {
+        header('location: ../index.php');
+    }
+
+?>
