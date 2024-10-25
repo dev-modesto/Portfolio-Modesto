a43ad40e603e14058394ab4e140b4dfe5df55110
Gabriel.Modesto <gabriel_26@outlook.com.br>
2024-10-25 14:28:53 -0300

    feat: criar função consulta autor e autor !== gabriel

--- include/funcoes/db-queries/autor.php
+++ include/funcoes/db-queries/autor.php
@@ -14,4 +14,35 @@ function cIdgabriel($con) {
     return $id;
 }
 
+function cAutorDiferenteGabriel($con) {
+    $sqlAutor = "SELECT * FROM tbl_autor WHERE NOT nome LIKE '%gabriel modesto%' ORDER BY nome ASC";
+    $consultaAutor = mysqli_query($con, $sqlAutor);
+    $arrayAutores = mysqli_fetch_all($consultaAutor, MYSQLI_ASSOC);
+    return $arrayAutores;
+}
+
+function cAutor($con, $idAutor = null) {
+
+    $where = 'WHERE 1=1';
+    $types = '';
+    $vars = [];
+
+    if (!empty($idAutor)) {
+        $where .= ' AND id_autor = ?';
+        $types .= 'i';
+        $vars[] = $idAutor;
+    }
+
+    $sql = mysqli_prepare($con, "SELECT * FROM tbl_autor $where");
+
+    if ($vars) {
+        mysqli_stmt_bind_param($sql, $types, ...$vars);
+    }
+
+    mysqli_stmt_execute($sql);
+    $consulta = mysqli_stmt_get_result($sql);
+    return $consulta;
+
+}
+
 ?>
