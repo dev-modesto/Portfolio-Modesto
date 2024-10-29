6201b43cb62e49324773845a4c090af900308136
dev-modesto <gabriel_26@outlook.com.br>
2024-10-29 18:53:05 -0300

    refactor: incluir const dentro do escopo da função

--- js/animacoes.js
+++ js/animacoes.js
@@ -1,8 +1,7 @@
-const section = document.querySelectorAll('.js-scroll');
-
-const windowMetade = window.innerHeight * .5;
-
 function animaScroll() {
+    const windowMetade = window.innerHeight * .5;
+    const section = document.querySelectorAll('.js-scroll');
+
     section.forEach((section) => {
         const topSection = section.getBoundingClientRect().top;
         const sectionVisible = (topSection - windowMetade) < 0;
